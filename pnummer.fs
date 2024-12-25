47049b95121aa733789ed70c3a813d7b6f11589e
Anosh Ullenius <git@anosh.se>
2024-12-25 13:23:53 +0100

    Refactor large word. Extract words

--- pnummer.fs
+++ pnummer.fs
@@ -2,10 +2,10 @@
 
 9 constant CHECK_DIGIT_POS
 
-: ten-mod 10 mod ;
+: 10mod 10 mod ;
 
 \ TODO better name
-: luhn ( sum -- n ) ten-mod negate 10 + ten-mod ; \ (10 - (sum mod 10)) mod 10
+: luhn ( sum -- n ) 10mod negate 10 + 10mod ; \ (10 - (sum mod 10)) mod 10
 
 : ascii2digit ( addr - n ) c@ 0x30 - ;
 
@@ -17,18 +17,19 @@
 
 variable sum
 
+: ignore-check-digit ( addr u -- addr u2 ) 1 - ;
+
+: double-if-even ( n -- n2 ) even if double one-digit then ;
+
 : iter ( addr u -- sum ) \ luhn-digit sum
-    1 - \ ignore check digit
+    ignore-check-digit
     0 do dup i + ascii2digit
-    i even if \ double every second digit
-        double one-digit
-    then
+    i double-if-even
     sum +! loop
 drop sum @
 ;
 
 : last-digit ( addr u -- n ) drop CHECK_DIGIT_POS + ascii2digit ;
-
 
 : main
     \ TODO validate argc
@@ -37,11 +38,7 @@ drop sum @
     >r last-digit r>
     = \ compare calculated digit to actual
     invert
-\    .s \ debug
     (bye)
 ;
 
  main
-
-\ (bye) ( n -- ) - exit code to OS
-\ argc ( -- addr ) c@ 
